c2564f7b62c9d07256f8c59da0104681c0bef963
sam@dreen.eu <your_email_address@example.com>
2022-05-15 13:41:55 +0200

    points to questions and testpage adjustments

--- src/backend.js
+++ src/backend.js
@@ -5,19 +5,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Don’t dare to interrupt them"
+                text: "Don’t dare to interrupt them",
+                points: 0
             },
             {
                 option: "b",
-                text: "Think it’s more important to give them some of your time; work can wait"
+                text: "Think it’s more important to give them some of your time; work can wait",
+                points: 1
             },
             {
                 option: "c",
-                text: "Listen, but with only with half an ear"
+                text: "Listen, but with only with half an ear",
+                points: 2
             },
             {
                 option: "d",
-                text: "Interrupt and explain that you are really busy at the moment"
+                text: "Interrupt and explain that you are really busy at the moment",
+                points: 3
             },
         ]
     },
@@ -27,19 +31,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Look at your watch every two minutes"
+                text: "Look at your watch every two minutes",
+                points: 0
             },
             {
                 option: "b",
-                text: "Bubble with inner anger, but keep quiet"
+                text: "Bubble with inner anger, but keep quiet",
+                points: 1
             },
             {
                 option: "c",
-                text: "Explain to other equally impatient people in the room that the doctor is always running late"
+                text: "Explain to other equally impatient people in the room that the doctor is always running late",
+                points: 2
             },
             {
                 option: "d",
-                text: "Complain in a loud voice, while tapping your foot impatiently"
+                text: "Complain in a loud voice, while tapping your foot impatiently",
+                points: 3
             },
         ]
     },
@@ -49,19 +57,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Don’t dare contradict them"
+                text: "Don’t dare contradict them",
+                points: 0
             },
             {
                 option: "b",
-                text: "Think that they are obviously right"
+                text: "Think that they are obviously right",
+                points: 1
             },
             {
                 option: "c",
-                text: "Defend your own point of view, tooth and nail"
+                text: "Defend your own point of view, tooth and nail",
+                points: 2
             },
             {
                 option: "d",
-                text: "Continuously interrupt your colleague"
+                text: "Continuously interrupt your colleague",
+                points: 3
             },
         ]
     },
@@ -71,19 +83,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Are a bit too far towards the back so don’t really hear what the guide is saying"
+                text: "Are a bit too far towards the back so don’t really hear what the guide is saying",
+                points: 0
             },
             {
                 option: "b",
-                text: "Follow the group without question"
+                text: "Follow the group without question",
+                points: 1
             },
             {
                 option: "c",
-                text: "Make sure that everyone is able to hear properly"
+                text: "Make sure that everyone is able to hear properly",
+                points: 2
             },
             {
                 option: "d",
-                text: "Are right up the front, adding your own comments in a loud voice"
+                text: "Are right up the front, adding your own comments in a loud voice",
+                points: 3
             },
         ]
     },
@@ -93,19 +109,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Ask you to tell a story in front of everyone else"
+                text: "Ask you to tell a story in front of everyone else",
+                points: 0
             },
             {
                 option: "b",
-                text: "Talk privately between themselves"
+                text: "Talk privately between themselves",
+                points: 1
             },
             {
                 option: "c",
-                text: "Hang around you all evening"
+                text: "Hang around you all evening",
+                points: 2
             },
             {
                 option: "d",
-                text: "Always drag the conversation back to themselves"
+                text: "Always drag the conversation back to themselves",
+                points: 3
             },
         ]
     },
@@ -115,19 +135,23 @@ export const Backend = [
         answers: [
             {
                 option: "a",
-                text: "Think it’s for the best — it was a lame joke anyway"
+                text: "Think it’s for the best — it was a lame joke anyway",
+                points: 0
             },
             {
                 option: "b",
-                text: "Wait to share it with your friends after work"
+                text: "Wait to share it with your friends after work",
+                points: 1
             },
             {
                 option: "c",
-                text: "Try again a bit later with one of your colleagues"
+                text: "Try again a bit later with one of your colleagues",
+                points: 2
             },
             {
                 option: "d",
-                text: "Keep telling it until they pay attention"
+                text: "Keep telling it until they pay attention",
+                points: 3
             },
         ]
     },
--- src/containers/TestPage/TestPage.js
+++ src/containers/TestPage/TestPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import { Backend } from "../../backend";
 import { AnswerButton } from '../../components/AnswerButton/AnswerButton';
 
@@ -7,15 +7,27 @@ import "./TestPage.scss";
 
 export const TestPage = () => {
     const [selectedAnswer, setSelectedAnswer] = useState();
-    const questionNr = useParams().nr;
-    const questionData = Backend[questionNr];
+
+    const navigate = useNavigate();
+    const questionNr = parseInt(useParams().nr);
+
+    const questionData = Backend[questionNr - 1];
+    const nextQuestionHandler = () => {
+        if(questionNr < 6) {
+            setSelectedAnswer();
+            //Add logic to sum points
+            navigate(`/test/${questionNr + 1}`);
+        }
+    }
+
     return (
         <div className='test-page' >
             <p>Question {questionNr}/6</p>
             <h1 className='test-page__title'>{questionData.question}</h1>
             <div className='answer-container' >
-                {questionData.answers.map(answer =>
+                {questionData.answers.map((answer, i) =>
                     <AnswerButton
+                        key={i}
                         option={answer.option}
                         answer={answer.text}
                         selectedAnswer={selectedAnswer}
@@ -23,8 +35,8 @@ export const TestPage = () => {
                     />)
                 }
             </div>
-            <button className='next-question-button'> 
-                Next question &gt;
+            <button onClick={() => nextQuestionHandler()} disabled={!selectedAnswer} className='next-question-button'>
+                {questionNr < 6 ? "Next question >" : "See results"}
             </button>
         </div>
     )
